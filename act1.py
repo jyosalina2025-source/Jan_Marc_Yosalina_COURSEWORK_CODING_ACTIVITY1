db04ca6f26f53a196148143f38251e4811ec345b
Jan Marc <jyosalina2025@student.nbscollege.edu.ph>
2026-02-05 09:25:23 +0800

    Add files via upload

---
+++ act1.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+age = int(age)
+
+next_year_age = age + 1
+
+print("Hello, " + name + ". You will be " + str(next_year_age) + " in one year.")
+
+print("Your name has " + str(len(name)) + " characters.")
