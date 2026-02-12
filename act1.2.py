f023855aaa063ed07935cd76b2e401a4d7b3d177
Jan Marc <jyosalina2025@student.nbscollege.edu.ph>
2026-02-12 20:38:20 +0800

    Fix variable name from 'Nunber' to 'Number'

--- act1.2.py
+++ act1.2.py
@@ -1,11 +1,12 @@
-Nunber = 4
+Number = 4
 
 while True:
     guess = int(input("Enter a number between 1 and 10: "))
-    if guess < Nunber:
+    if guess < Number:
         print("Too low!")
-    elif guess > Nunber:
+    elif guess > Number:
         print("Too high!")
     else:
         print("Congratulations! You've guessed the number.")
+
         break
